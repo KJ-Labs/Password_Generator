dba5de68b9b135afbb2749cc970d0b39f766ffed
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-06-30 22:00:52 -0700

    Updated to allow variables outside of fuctions

--- Develop/script.js
+++ Develop/script.js
@@ -1,73 +1,97 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate"); 
+var generateBtn = document.querySelector("#generate");
 
+const key_strings = {
+  lowercase: 'abcdefghijklmnopqrstuvwxyz',
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  number: '0123456789',
+  symbol: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+};
 
+function generatePassword2() {
+  var passwordCharSet = "";
 
-var max_count = 5;
-function showNumconfirm() {
-  var numbersinpwd = (prompt("Do you want numbers in your password? Yes or No", ""));
-  if (numbersinpwd.toLowerCase() === "yes" || numbersinpwd === "no") {
-    alert("You chose " + numbersinpwd.toLowerCase() + ' to adding numbers to your password.');
-  } else {
-    alert("You did not type Yes or No. Please try again");
-    // to limit user for limited count
-    if (--max_count > 0)
-      showNumconfirm()
+  var max_count = 5;
+
+  function showNumconfirm() {
+    var numbersinpwd = (prompt("Do you want numbers in your password? Yes or No", ""));
+    var answer = numbersinpwd.toLowerCase();
+    if (answer === "yes" || answer === "no") {
+      passwordCharSet += key_strings.number
+    } else {
+      alert("You did not type Yes or No. Please try again");
+      // to limit user for limited count
+      if (--max_count > 0)
+        showNumconfirm()
+    }
   }
+  showNumconfirm();
+
+  function showsymbconfirm() {
+    var symbinpwd = (prompt("Do you want special characters in your password? Yes or No", ""));
+    var answer = symbinpwd.toLowerCase();
+    if (answer === "yes" || answer === "no") {
+      passwordCharSet += key_strings.symbol;
+    } else {
+      alert("You did not type Yes or No. Please try again");
+      // to limit user for limited count
+      if (--max_count > 0)
+        showsymbconfirm()
+    }
+  }
+  showsymbconfirm();
+
+  function uppercaseconfirm() {
+    var uppercaseinpwd = (prompt("Do you want uppercase letters in your password? Yes or No", ""));
+    var answer = uppercaseinpwd.toLowerCase();
+    if (answer === "yes" || answer === "no") {
+      passwordCharSet += key_strings.uppercase;
+    } else {
+      alert("You did not type Upper or Lower. Please try again");
+      // to limit user for limited count
+      if (--max_count > 0)
+        uppercaseconfirm()
+    }
+  }
+  uppercaseconfirm();
+
+  function lowercaseconfirm() {
+    var uppercaseinpwd = (prompt("Do you want lowercase letters in your password? Yes or No", ""));
+    var answer = uppercaseinpwd.toLowerCase();
+    if (answer === "yes" || answer === "no") {
+      passwordCharSet += key_strings.lowercase;
+    } else {
+      alert("You did not type Upper or Lower. Please try again");
+      // to limit user for limited count
+      if (--max_count > 0)
+        lowercaseconfirm()
+    }
+  }
+ 
+
+  var length = window.prompt("Enter a number from 8 to 128 for password length.");
+
+
+  var password = "";
+  for (let i = 0; i < length; i++) {
+    password += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
+  }
+  return password;
 }
-showNumconfirm();
-function showSymbconfirm() {
-  var symbinpwd = (prompt("Do you want special characters in your password? Yes or No", ""));
-  if (symbinpwd.toLowerCase() === "yes" || symbinpwd === "no") {
-    alert("You chose " + symbinpwd.toLowerCase() + ' to adding special characters to your password.');
-  } else {
-    alert("You did not type Yes or No. Please try again");
-    // to limit user for limited count
-    if (--max_count > 0)
-      showSymbconfirm()
-  }
+
+console.log(generatePassword2());
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword2();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
-showSymbconfirm();
-function uppercaseconfirm() {
-  var upperOrLowerinPwd = (prompt("Do you want only upper case or only lowercase in your password? Uppercase or Lowercase", ""));
-  if (upperOrLowerinPwd.toLowerCase() === "uppercase" || upperOrLowerinPwd === "lowercase") {
-    alert("You chose " + upperOrLowerinPwd.toLowerCase() + ' letters for your password.');
-  } else {
-    alert("You did not type Uppercase or Lowercase. Please try again");
-    // to limit user for limited count
-    if (--max_count > 0)
-      uppercaseconfirm()
-  }
-}
-uppercaseconfirm();
-function pwdLengthconfirm() {
-  var pwdLengthnum = (prompt("Please tell me how long you want your password to be. It can be 8 to 128 characters long", ""));
-  if (pwdLengthnum <= 128 && pwdLengthnum >= 8) {
-    alert("Your password length " + pwdLengthnum + " matches the requirements.");
-  } else {
-    alert("The number you input " + pwdLengthnum + " is not between 8 and 128");
-    // to limit user for limited count
-    if (--max_count > 0)
-      pwdLengthconfirm()
-  }
-}
-  pwdLengthconfirm();
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
-
-
-
-
-
-
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
-  }
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
